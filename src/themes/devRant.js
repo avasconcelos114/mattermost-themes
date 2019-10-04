63186ed944279e3a525a853b81c79490a7e6f601
Joram Wilander <jwawilander@gmail.com>
2019-10-04 12:38:03 -0400

    Proposal to change devRant online indicator color to green

--- src/themes/devRant.js
+++ src/themes/devRant.js
@@ -2,7 +2,7 @@ export default {
     name: 'devRant',
     thumbnailUrl: 'devRant.png',
     type: 'dark',
-    theme: {'sidebarBg':'#383952','sidebarText':'#ffffff','sidebarUnreadText':'#ffffff','sidebarTextHoverBg':'#f99a66','sidebarTextActiveBorder':'#f99a66','sidebarTextActiveColor':'#ffffff','sidebarHeaderBg':'#54556e','sidebarHeaderTextColor':'#ffffff','onlineIndicator':'#c14857','awayIndicator':'#e0b333','dndIndicator':'#f74343','mentionBj':'#c14857','mentionColor':'#ffffff','centerChannelBg':'#54556e','centerChannelColor':'#ffffff','newMessageSeparator':'#f80','linkColor':'#aaaab8','buttonBg':'#26a970','buttonColor':'#ffffff','errorTextColor':'#fd5960','mentionHighlightBg':'#c14857','mentionHighlightLink':'#e0b333','codeTheme':'solarized-dark','mentionBg':'#c14857'}
+    theme: {'sidebarBg':'#383952','sidebarText':'#ffffff','sidebarUnreadText':'#ffffff','sidebarTextHoverBg':'#f99a66','sidebarTextActiveBorder':'#f99a66','sidebarTextActiveColor':'#ffffff','sidebarHeaderBg':'#54556e','sidebarHeaderTextColor':'#ffffff','onlineIndicator':'#48c14a','awayIndicator':'#e0b333','dndIndicator':'#f74343','mentionBj':'#c14857','mentionColor':'#ffffff','centerChannelBg':'#54556e','centerChannelColor':'#ffffff','newMessageSeparator':'#f80','linkColor':'#aaaab8','buttonBg':'#26a970','buttonColor':'#ffffff','errorTextColor':'#fd5960','mentionHighlightBg':'#c14857','mentionHighlightLink':'#e0b333','codeTheme':'solarized-dark','mentionBg':'#c14857'}
 }
 
 
